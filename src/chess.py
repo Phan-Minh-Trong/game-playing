a5284eb54f9b3a39d4a51a232ee8849209330bf8
Trong Phan <127284415+FanMinkJonk@users.noreply.github.com>
2025-11-23 00:27:46 +0700

    Added basic structure

---
+++ src/chess.py
@@ -0,0 +1,18 @@
+
+class Piece:
+    def __init__(self):
+        pass
+
+class Board:
+    def __init__(self):
+        pass
+    
+    def init_board(self):
+        pass
+
+class Chess:
+    def __init__(self):
+        pass
+    
+    def run_chess(self):
+        pass
---
+++ src/main.py
@@ -0,0 +1,12 @@
+from src.chess import Chess
+
+
+
+def main():
+    game = Chess 
+    print("--- Chess game ---")
+    game.run_chess()
+    
+    
+if __name__ == "__main__":
+    main()
